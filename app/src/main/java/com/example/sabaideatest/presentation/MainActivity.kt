0983990a9f4771531d31e6dd75bbbdd601a10edd
Ali Kazemi <Alikazemi.developer@gmail.com>
2023-04-25 08:44:47 +0430

    change setupNavigation in MainActivity

--- app/src/main/java/com/example/sabaideatest/presentation/MainActivity.kt
+++ app/src/main/java/com/example/sabaideatest/presentation/MainActivity.kt
@@ -1,25 +1,24 @@
 package com.example.sabaideatest.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.example.sabaideatest.R
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupNavigation()
-
     }
 
     private fun setupNavigation() {
-        navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
     }
 }
